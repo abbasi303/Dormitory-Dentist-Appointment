3fc1621ea26a2675b229b0c66ff27b72ce068226
abbasi303 <usalcgg18@gmail.com>
2022-06-10 09:30:33 +0200

    Hi

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+        Room r=new Room(13);
+        r.addNewStudent();
+        r.addNewStudent();
+        r.addNewStudent();
+        r.removeStudent();
+        System.out.println(r.getRoomNumber());
+        System.out.println(r.getNumberOfStudents());
+    }
+
+
 }
